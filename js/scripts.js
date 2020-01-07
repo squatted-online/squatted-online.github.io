5b564c19a50166b53c657e3b088701b90b62fc63
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2020-01-07 21:52:23 +0100

    update scripts

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,6 @@
 
 const full = window.location.host
 const parts = full.split('.')
-const sub = parts[0]
+const sub = parts[0];
 
-console.log(sub)
-
-
-const iframe_container = document.querySelector('.iframe-bg')
-
-console.log(iframe_container)
-
-if (sub.length > 0) {
-    // if there is a subdomain
-    const url = 'https://www.' + sub + '.com';
-    let iframe = document.createElement('iframe');
-    iframe.src = url;
-    iframe_container.appendChild(iframe);
-}
+console.log(sub);
