150a6f88ee93526b9174d8cd836eb7da64ca6992
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2020-02-20 14:25:05 +0100

    add http://www.pawsmenu.net/ to squat

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 // document.designMode = "on"
+const iframe = document.querySelector('#pawsmenu iframe')
+iframe.setAttribute('width', innerWidth)
+iframe.setAttribute('height', innerHeight)
+console.log(iframe);
+document.body.onresize = () => {
+    iframe.setAttribute('width', innerWidth)
+    iframe.setAttribute('height', innerHeight)
+}
